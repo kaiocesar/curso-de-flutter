12d4c8dddce38eb59b5b999ec0823ee9f5eaaf1a
Kaio Cesar <tecnico.kaio@gmail.com>
2020-02-24 16:42:33 -0300

    Alterando a visibilidade da classe do widget

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -4,25 +4,43 @@ main() {
   runApp(PerguntaApp());
 }
 
-class PerguntaApp extends StatelessWidget {
-  void resposta() {
+// essa classe irá gerenciar o estado
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+
+  void _resposta() {
+    setState(() {
+      _perguntaSelecionada++;  
+    });
     print('Essa é a resposta 1');
   }
 
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Quantos planetas existe no sistema solar?',
+      'Qual a quantidade de satelites naturais que Jupiter possui?'
+    ];
+
     return MaterialApp(
       home: Scaffold(appBar: AppBar(
         actions: <Widget>[], 
         title: Text('Perguntas')),
         body: Column(
           children: <Widget>[
-            Text('Primeira pergunta'),
+            Text(perguntas[_perguntaSelecionada]),
             RaisedButton(
               child: Text('Resposta 1'),
-              onPressed: resposta,
+              onPressed: _resposta, 
             )
         ],)
         ,)
     );
-  }
+  }  
+}
+
+// essa classe irá gerenciar o widget
+class PerguntaApp extends StatefulWidget {
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
+  }  
 }
