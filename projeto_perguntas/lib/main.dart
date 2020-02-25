206a19f8ae314603154b33e04ca94a898166e213
Kaio Cesar <tecnico.kaio@gmail.com>
2020-02-25 20:59:03 -0300

    componente resposta.dart

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import './questao.dart';
-
+import './resposta.dart';
 
 main() {
   runApp(PerguntaApp());
@@ -30,10 +30,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
         body: Column(
           children: <Widget>[
             Questao(perguntas[_perguntaSelecionada]),
-            RaisedButton(
-              child: Text('Resposta 1'),
-              onPressed: _resposta, 
-            )
+            Resposta('Resposta')
         ],)
         ,)
     );
